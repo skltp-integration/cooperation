a0fc17d4664373365011d584bfcad4c43b80cd6c
Peter Merikan <peter@merikan.com>
2015-07-14 12:42:25 +0200

    Load h2 console when spring profile dev is active

--- src/main/java/se/skltp/cooperation/CooperationApplication.java
+++ src/main/java/se/skltp/cooperation/CooperationApplication.java
@@ -1,12 +1,25 @@
 package se.skltp.cooperation;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class CooperationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CooperationApplication.class, args);
+    }
+
+
+    @Bean
+    @Profile("dev")
+    public ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+        registration.addUrlMappings("/console/*");
+        return registration;
     }
 }
