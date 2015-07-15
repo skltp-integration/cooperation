adf79531e71ca5e06d646ab5e7238d0f11cc6e95
Peter Merikan <peter@merikan.com>
2015-07-15 18:38:29 +0200

    Update test to handle dto's and change it to unit instead of integration

--- src/test/java/se/skltp/cooperation/web/rest/v1/ConnectionPointControllerTest.java
+++ src/test/java/se/skltp/cooperation/web/rest/v1/ConnectionPointControllerTest.java
@@ -1,29 +1,39 @@
 package se.skltp.cooperation.web.rest.v1;
 
+import org.dozer.DozerBeanMapper;
+import org.dozer.Mapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.transaction.annotation.Transactional;
 import se.skltp.cooperation.CooperationApplication;
 import se.skltp.cooperation.domain.ConnectionPoint;
 import se.skltp.cooperation.repository.ConnectionPointRepository;
 import se.skltp.cooperation.web.rest.v1.controller.ConnectionPointController;
+import se.skltp.cooperation.web.rest.v1.dto.ConnectionPointDTO;
 
 import javax.annotation.PostConstruct;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -37,72 +47,87 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = CooperationApplication.class)
 @WebAppConfiguration
-@IntegrationTest
 public class ConnectionPointControllerTest {
 
-    @Autowired
-    private ConnectionPointRepository connectionPointRepository;
-
-    private MockMvc restConnectionPointMockMvc;
+    @Mock
+    private ConnectionPointRepository connectionPointRepositoryMock;
+    @Mock
+    private DozerBeanMapper mapperMock;
+    @InjectMocks
+    ConnectionPointController uut;
 
     private ConnectionPoint cp1;
     private ConnectionPoint cp2;
+    private MockMvc mockMvc;
 
     @PostConstruct
     public void setup() {
         MockitoAnnotations.initMocks(this);
-        ConnectionPointController uut = new ConnectionPointController();
-        ReflectionTestUtils.setField(uut, "connectionPointRepository", connectionPointRepository);
-        this.restConnectionPointMockMvc = MockMvcBuilders.standaloneSetup(uut).build();
+        this.mockMvc = MockMvcBuilders.standaloneSetup(uut).build();
     }
 
-    @Before
-    public void initTest() {
-        cp1 = new ConnectionPoint();
-        cp1.setPlatform("cp1.platform");
-        cp1.setEnvironment("cp1.environment");
-        cp2 = new ConnectionPoint();
-        cp2.setPlatform("cp2.platform");
-        cp2.setEnvironment("cp2.environment");
-    }
 
     @Test
-    @Transactional
     public void getAll_shouldReturnAll() throws Exception {
-        // Initialize the database
-        connectionPointRepository.saveAndFlush(cp1);
-        connectionPointRepository.saveAndFlush(cp2);
+
+        ConnectionPoint cp1 = new ConnectionPoint();
+        ConnectionPoint cp2 = new ConnectionPoint();
+        ConnectionPointDTO dto1 = new ConnectionPointDTO();
+        dto1.setId(1L);
+        dto1.setPlatform("dt01.platform");
+        dto1.setEnvironment("dto1.environment");
+        ConnectionPointDTO dto2 = new ConnectionPointDTO();
+        dto2.setId(2L);
+        dto2.setPlatform("dt02.platform");
+        dto2.setEnvironment("dto2.environment");
+
+        when(connectionPointRepositoryMock.findAll()).thenReturn(Arrays.asList(cp1, cp2));
+        when(mapperMock.map(cp1, ConnectionPointDTO.class)).thenReturn(dto1);
+        when(mapperMock.map(cp2, ConnectionPointDTO.class)).thenReturn(dto2);
 
         // Get all the connectionPoints
-        restConnectionPointMockMvc.perform(get("/v1/connectionPoints"))
+        mockMvc.perform(get("/v1/connectionPoints"))
             .andExpect(status().isOk())
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(jsonPath("$", hasSize(2)))
-            .andExpect(jsonPath("$.[0].id").value(is(cp1.getId().intValue())))
-            .andExpect(jsonPath("$.[0].platform").value(is(cp1.getPlatform())))
-            .andExpect(jsonPath("$.[0].environment").value(is(cp1.getEnvironment())));
+            .andExpect(jsonPath("$.[0].id").value(is(dto1.getId().intValue())))
+            .andExpect(jsonPath("$.[0].platform").value(is(dto1.getPlatform())))
+            .andExpect(jsonPath("$.[0].environment").value(is(dto1.getEnvironment())))
+            .andExpect(jsonPath("$.[1].id").value(is(dto2.getId().intValue())))
+            .andExpect(jsonPath("$.[1].platform").value(is(dto2.getPlatform())))
+            .andExpect(jsonPath("$.[1].environment").value(is(dto2.getEnvironment())));
+
+        verify(connectionPointRepositoryMock, times(1)).findAll();
+        verifyNoMoreInteractions(connectionPointRepositoryMock);
+
     }
+
     @Test
-    @Transactional
     public void getAll_shouldReturnEmptyList() throws Exception {
-        // Initialize the database
-        connectionPointRepository.deleteAll();
+
+        when(connectionPointRepositoryMock.findAll()).thenReturn(Collections.emptyList());
 
         // Get all the connectionPoints
-        restConnectionPointMockMvc.perform(get("/v1/connectionPoints"))
+        mockMvc.perform(get("/v1/connectionPoints"))
             .andExpect(status().isOk())
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(jsonPath("$", hasSize(0)));
     }
 
     @Test
-    @Transactional
     public void get_shouldReturnOne() throws Exception {
-        // Initialize the database
-        connectionPointRepository.saveAndFlush(cp1);
+        ConnectionPoint cp1 = new ConnectionPoint();
+        cp1.setId(1L);
+        ConnectionPointDTO dto1 = new ConnectionPointDTO();
+        dto1.setId(1L);
+        dto1.setPlatform("dt01.platform");
+        dto1.setEnvironment("dto1.environment");
+
+        when(connectionPointRepositoryMock.findOne(cp1.getId())).thenReturn(cp1);
+        when(mapperMock.map(cp1, ConnectionPointDTO.class)).thenReturn(dto1);
 
         // Get the cp1
-        restConnectionPointMockMvc.perform(get("/v1/connectionPoints/{id}", cp1.getId()))
+        mockMvc.perform(get("/v1/connectionPoints/{id}", cp1.getId()))
             .andExpect(status().isOk())
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
             .andExpect(jsonPath("$.id").value(cp1.getId().intValue()))
@@ -111,10 +136,11 @@ public class ConnectionPointControllerTest {
     }
 
     @Test
-    @Transactional
     public void get_shouldReturnNotFound() throws Exception {
-        connectionPointRepository.deleteAll();
-        restConnectionPointMockMvc.perform(get("/v1/connectionPoints/{id}", Long.MAX_VALUE))
+
+        when(connectionPointRepositoryMock.findOne(anyLong())).thenReturn(null);
+
+        mockMvc.perform(get("/v1/connectionPoints/{id}", Long.MAX_VALUE))
             .andExpect(status().isNotFound());
     }
 
