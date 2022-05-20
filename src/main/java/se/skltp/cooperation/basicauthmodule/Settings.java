473f66d7da1fd3979b7b73864185e42c46fe51cd
Henrik Augustsson <henrik.augustsson@nordicmedtest.se>
2022-05-20 11:29:38 +0200

    Some comments.

--- src/main/java/se/skltp/cooperation/basicauthmodule/Settings.java
+++ src/main/java/se/skltp/cooperation/basicauthmodule/Settings.java
@@ -1,17 +1,23 @@
 package se.skltp.cooperation.basicauthmodule;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Settings {
 
 	// User file settings:
 
 	/** Absolute or relative folder path. Used to check if the folder exists, and create it if not. */
+	// TODO: Implement @Value
 	static final String folderPath = "./config/";
+	// TODO: Implement @Value
 	/** Filename with .json file type. */
 	static final String fileName = "userlist.json";
 	/** Absolute or relative file path. Used for file read and write. Concatenated from other values. */
 	static final String filePath = folderPath + fileName;
 	/** A CRON temporal expression for how often the user file should be read.<br>
 	 * Currently: "At 5 minutes past the hour, every hour, every day." */
+	// TODO: Implement @Value("") to not have to build on cron-timing changes.
+	// @Value("ewq", default="qwe")
 	static final String fileReadCron = "0 5 * * * *";
 
 	// API settings:
