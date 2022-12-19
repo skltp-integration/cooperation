e848a53975ea13666956bcaadf048452dddbe045
Björn Pettersson <bjorn.pettersson@nordicmedtest.se>
2022-12-19 09:46:40 +0100

    Script image draft

--- scripts/cooperation/cooperation-import-from-tak-env-setup.sh
+++ scripts/cooperation/cooperation-import-from-tak-env-setup.sh
@@ -5,11 +5,11 @@
 #-----------------------------
 # Cooperation config
 #-----------------------------
-coopJdbcUrl=jdbc:mysql://localhost/cooperation
-coopJdbcUser=coop_user
-coopJdbcPassword=coop_pw
+coopJdbcUrl=${COOPERATION_DB_URL}
+coopJdbcUser=${COOPERATION_DB_USER}
+coopJdbcPassword=${COOPERATION_DB_PASSWORD}
 
-coop_auth_user_and_pass=Caesar:qwerty
+coop_auth_user_and_pass=${COOPERATION_AUTH_USER_AND_PASS}
 
 tmpDir=/tmp/cooperation-import-from-tak
 
@@ -19,15 +19,15 @@ coopImportFilesDir=${tmpDir}/import
 # Cooperation config validering
 #-----------------------------
 dump_files=(ntjp_test ntjp_qa ntjp_prod sll_qa sll_prod)
-connection_points_url=http://<cooperation_url>/coop/api/v1/connectionPoints
+connection_points_url=${COOPERATION_CONNECTION_POINTS_URL}
 
 #-----------------------------
 # SFTP settings for getting TAK data
 #-----------------------------
-sftpHost=ine-pib-misc01.sth.basefarm.net
-sftpUser=coop-user
-sftpRemotePath="/upload/"
-sshIdentityFile=~/.ssh/id_rsa_tak_data_transfer
+sftpHost=${COOPERATION_SFTP_HOST}
+sftpUser=${COOPERATION_SFTP_USER}
+sftpRemotePath=${COOPERATION_SFTP_PATH}
+sshIdentityFile=${COOPERATION_SFTP_KEYFILE}
 
 #-----------------------------
 # Script environment config
