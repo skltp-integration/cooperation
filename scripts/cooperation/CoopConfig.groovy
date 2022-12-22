117a309e270e966910510f6a0994b0e337b2e7ea
Björn Pettersson <bjorn.pettersson@nordicmedtest.se>
2022-12-22 14:48:13 +0100

    CoopConfig from system env

--- scripts/cooperation/CoopConfig.groovy
+++ scripts/cooperation/CoopConfig.groovy
@@ -1,1 +1,2 @@
-environments = ["ntjp_prod", "sll_prod", "ntjp_qa", "sll_qa", "ntjp_test", "ntjp_dev"]
+//environments = ["ntjp_prod", "sll_prod", "ntjp_qa", "sll_qa", "ntjp_test", "ntjp_dev"]
+environments = System.getenv('COOPERATION_IMPORT_ENVIRONMENTS').tokenize(',')
