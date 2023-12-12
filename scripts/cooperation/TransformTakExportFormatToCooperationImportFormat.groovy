34cc88343e9abaf3acdaf587dc23d087fcd06a5c
Henrik Augustsson <henrik.augustsson@nordicmedtest.se>
2023-12-12 11:17:01 +0100

    added grapes to the grape vine for groovy files, relating to ECS logging imports.

--- scripts/cooperation/TransformTakExportFormatToCooperationImportFormat.groovy
+++ scripts/cooperation/TransformTakExportFormatToCooperationImportFormat.groovy
@@ -5,8 +5,9 @@
 
 @Grapes([
 	@GrabConfig(systemClassLoader = true),
-	@Grab(group = 'ch.qos.logback', module = 'logback-classic', version = '1.2.3'),
-	@Grab(group = 'net.logstash.logback', module = 'logstash-logback-encoder', version = '6.4')
+	@Grab(group = 'ch.qos.logback',       module = 'logback-classic',          version = '1.2.3'),
+	@Grab(group = 'net.logstash.logback', module = 'logstash-logback-encoder', version = '6.4'),
+	@Grab(group = 'co.elastic.logging',   module = 'logback-ecs-encoder',      version = '1.5.0'),
 ])
 
 import groovy.json.*
--- scripts/logback-ecs.groovy
+++ scripts/logback-ecs.groovy
@@ -1,3 +1,9 @@
+#!/usr/bin/env groovy
+
+@Grapes([
+	@Grab(group = 'co.elastic.logging', module = 'logback-ecs-encoder', version = '1.5.0'),
+])
+
 import co.elastic.logging.logback.EcsEncoder;
 appender("STDOUT", ConsoleAppender) {
   encoder(EcsEncoder) {}
