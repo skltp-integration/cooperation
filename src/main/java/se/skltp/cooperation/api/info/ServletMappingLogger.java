1c015c5007c078ec9da5e7727db8d47847d31794
holgerAtInera <Rickard.Holgersson@inera.se>
2025-11-20 12:00:00 +0100

    Merge pull request #21 from skltp-integration/feature/NTU-634-h2-console
    Feature/ntu 634 h2 console

---
+++ src/main/java/se/skltp/cooperation/api/info/ServletMappingLogger.java
@@ -0,0 +1,43 @@
+package se.skltp.cooperation.api.info;
+
+import jakarta.servlet.ServletContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServletMappingLogger implements ApplicationListener<ApplicationReadyEvent> {
+
+	private final Logger log = LoggerFactory.getLogger(ServletMappingLogger.class);
+
+	private final ServletContext servletContext;
+
+	public ServletMappingLogger(ServletContext servletContext) {
+		this.servletContext = servletContext;
+	}
+
+	@Override
+	public void onApplicationEvent(@NonNull ApplicationReadyEvent event) {
+		if (!log.isDebugEnabled()) {
+			return;
+		}
+		var servletRegistrations = servletContext.getServletRegistrations();
+		final String contextPath = getContextPath();
+		if (!servletRegistrations.isEmpty()) {
+			log.debug("Servlets:");
+			servletContext.getServletRegistrations().forEach((name, registration) ->
+				registration.getMappings().forEach(mapping -> log.debug("  {}{} -> {}", contextPath, mapping, name)
+			));
+		}
+	}
+	private String getContextPath() {
+		String contextPath = servletContext.getContextPath();
+		if (contextPath.endsWith("/")) {
+			contextPath = contextPath.substring(0, contextPath.length() - 1);
+		}
+		return contextPath;
+	}
+}
