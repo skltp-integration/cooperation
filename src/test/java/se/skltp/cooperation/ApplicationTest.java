035adc2f1b7b9ece05537da2b10f4680f52e2064
Henrik A. <89081762+HenrikAugustssonNMT@users.noreply.github.com>
2026-06-30 13:16:20 +0200

    - updated spring boot 3.5.12 to 3.5.16. (#30)
    - smaller updates to jacoco, ecs-logging, maven-release.
    - Modernized Mycila license plugin from 1.10 to 5.0.0 (10 year jump). It had been left on deprecated source-coordinates.
    -- Re-did license setup and automation.
    -- Ran plugin to refresh license headers.
    -- Also touched up license markdown file.
    
    Co-authored-by: Henrik Aug. <henrik.augustsson@inera.se>
    (cherry picked from commit 22ffd29e088534191b257f5bfe46064de83c3ca5)

--- src/test/java/se/skltp/cooperation/ApplicationTest.java
+++ src/test/java/se/skltp/cooperation/ApplicationTest.java
@@ -1,7 +1,9 @@
-/**
- * Copyright (c) 2015-2026 Inera.
- * * This library is free software under the GNU Lesser General Public License v2.1.
- * Refer to the full license files at the project root.
+/*
+ * Copyright © 2015-2026 Inera.
+ * Copyright owner URL: https://www.inera.se/
+ * TAK-API (Cooperation) overview page: https://inera.atlassian.net/wiki/spaces/NTJPP/pages/3359539201/Ineras+Informationstj+nster
+ * This library is free software under the GNU Lesser General Public License v2.1 or later.
+ * Please refer to the full license files at the project root.
  */
 package se.skltp.cooperation;
 
